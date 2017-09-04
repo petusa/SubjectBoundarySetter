938f3aac276c8bfd30b5b55880b485dcc5543e4d
petusa <peter.x.nagy@gmail.com>
2017-09-04 12:18:06 +0400

    Showing the recently set images, device specific folder is used

--- app/src/main/java/com/github/petusa/subjectboundarysetter/MainActivity.java
+++ app/src/main/java/com/github/petusa/subjectboundarysetter/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends com.github.ma1co.pmcademo.app.MainActivity {
     {
         activities = new ActivityListItem[] {
                 new ActivityListItem(R.string.title_activity_setter, SetterActivity.class),
+                new ActivityListItem(R.string.title_activity_playback_recent, PlaybackRecentActivity.class),
                 new ActivityListItem(R.string.title_activity_camera, CameraActivity.class),
                 new ActivityListItem(R.string.title_activity_playback, PlaybackActivity.class)
         };
